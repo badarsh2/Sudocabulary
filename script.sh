0beccfb5546e45f134108d39574ac1442533eba6
Adrian <totcha@gazeta.pl>
2016-08-09 09:21:07 +0200

    makes the script work with zsh on osx
    $OSTYPE check prevents this script from working on zsh in osx.

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 wget -O ~/.vocab "http://bit.ly/1WinY8l"
 if [[ "$OSTYPE" == "linux-gnu" && $(echo $0) == "bash" ]]; then
 	OSBASHRC=bashrc
-elif [[ "$OSTYPE" == "linux-gnu" && $(echo $0 == "zsh") ]]; then
+elif [[ $(echo $0 == "zsh") ]]; then
 	OSBASHRC=zshrc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   OSBASHRC=bash_profile
