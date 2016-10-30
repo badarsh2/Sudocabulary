7bde79990fa44c9d1b6a44243c7c71e384dd3661
srivatsan-ramesh <sriramesh4@gmail.com>
2016-10-30 09:24:07 +0530

    added wrong file previously

--- script.sh
+++ script.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env bash
+wget -O ~/.vocab "https://goo.gl/N4EiTq"
+wget -O ~/.vocabscript "https://goo.gl/gI7xKQ"
 if [[ "$OSTYPE" == "linux-gnu" && $(echo $0) == "bash" ]]; then
 	OSBASHRC=bashrc
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  OSBASHRC=bash_profile
 elif [[ $(echo $0 == "zsh") ]]; then
 	OSBASHRC=zshrc
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  OSBASHRC=bash_profile
 fi
 echo "" >> ~/.$OSBASHRC
 echo 'chmod +x ~/.vocab' >> ~/.$OSBASHRC
